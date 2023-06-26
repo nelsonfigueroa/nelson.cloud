bc417af059cdd08a52d89924fe422d4e748ec3d7
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-06-26 01:59:34 -0700

    removing mask javascript

--- themes/LoveIt/assets/js/theme.js
+++ themes/LoveIt/assets/js/theme.js
@@ -276,6 +276,9 @@ window.goatcounter = {endpoint: 'https://nelsonfigueroa.goatcounter.com/count'}
 })();
 // end goatcounter
 
+
+// Main theme javascript
+
 function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
 
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
@@ -326,6 +329,7 @@ var Util = /*#__PURE__*/function () {
       return window.matchMedia('only screen and (max-width: 960px)').matches;
     }
   }, {
+    // removing things here broke stuff. animateCSS has been removed though
     key: "animateCSS",
     value: function animateCSS(element, animation, reserved, callback) {
       var _element$classList;
@@ -636,27 +640,6 @@ var Theme = /*#__PURE__*/function () {
       }, false);
     }
   }, {
-    key: "onClickMask",
-    value: function onClickMask() {
-      var _this14 = this;
-
-      document.getElementById('mask').addEventListener('click', function () {
-        var _iterator4 = _createForOfIteratorHelper(_this14.clickMaskEventSet),
-            _step4;
-
-        try {
-          for (_iterator4.s(); !(_step4 = _iterator4.n()).done;) {
-            var event = _step4.value;
-            event();
-          }
-        } catch (err) {
-          _iterator4.e(err);
-        } finally {
-          _iterator4.f();
-        }
-      }, false);
-    }
-  }, {
     key: "init",
     value: function init() {
       var _this15 = this;
@@ -678,8 +661,6 @@ var Theme = /*#__PURE__*/function () {
         _this15.onScroll();
 
         _this15.onResize();
-
-        _this15.onClickMask();
       }, 100);
     }
   }]);
