9e66e62d03eeb12a3a3d9d6337ac99284ff0d14e
nelsonfigueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2024-11-24 23:54:50 -0800

    cleaning up html and css

--- themes/PaperMod/assets/js/fastsearch.js
+++ themes/PaperMod/assets/js/fastsearch.js
@@ -88,7 +88,7 @@ sInput.onkeyup = function (e) {
             let resultSet = ''; // our results bucket
 
             for (let item in results) {
-                resultSet += `<li class="post-entry"><header class="entry-header">${results[item].item.title}&nbsp;»</header>` +
+                resultSet += `<li class="post-entry"><header class="">${results[item].item.title}&nbsp;»</header>` +
                     `<a href="${results[item].item.permalink}" aria-label="${results[item].item.title}"></a></li>`
             }
 
