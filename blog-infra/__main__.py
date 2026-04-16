9b8b2485c993d053fa1af0454678ce94c82c7f54
nelsonfigueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2026-04-16 14:37:31 -0700

    formatting/linting

--- blog-infra/__main__.py
+++ blog-infra/__main__.py
@@ -7,7 +7,8 @@ import pulumi_aws as aws
 us_west_1 = aws.Provider("us-west-1", region="us-west-1")
 
 # CloudFront Function to strip /gc prefix for GoatCounter proxy
-goatcounter_rewrite = aws.cloudfront.Function("goatcounter-rewrite",
+goatcounter_rewrite = aws.cloudfront.Function(
+    "goatcounter-rewrite",
     name="goatcounter-rewrite",
     runtime="cloudfront-js-2.0",
     code="""\
@@ -21,7 +22,8 @@ function handler(event) {
 )
 
 # S3 bucket for nelson.cloud site files
-bucket = aws.s3.Bucket("nelson-cloud-bucket",
+bucket = aws.s3.Bucket(
+    "nelson-cloud-bucket",
     bucket="nelson.cloud",
     opts=pulumi.ResourceOptions(
         provider=us_west_1,
@@ -29,32 +31,38 @@ bucket = aws.s3.Bucket("nelson-cloud-bucket",
 )
 
 # IAM users
-hugo_user = aws.iam.User("hugo",
+hugo_user = aws.iam.User(
+    "hugo",
     name="hugo",
 )
 
-personal_laptop_user = aws.iam.User("personal-laptop",
+personal_laptop_user = aws.iam.User(
+    "personal-laptop",
     name="personal-laptop",
 )
 
 # IAM policy attachments for IAM users
-personal_laptop_admin = aws.iam.UserPolicyAttachment("personal-laptop-admin",
+personal_laptop_admin = aws.iam.UserPolicyAttachment(
+    "personal-laptop-admin",
     user=personal_laptop_user.name,
     policy_arn="arn:aws:iam::aws:policy/AdministratorAccess",
 )
 
-hugo_s3_full_access = aws.iam.UserPolicyAttachment("hugo-s3-full-access",
+hugo_s3_full_access = aws.iam.UserPolicyAttachment(
+    "hugo-s3-full-access",
     user=hugo_user.name,
     policy_arn="arn:aws:iam::aws:policy/AmazonS3FullAccess",
 )
 
-hugo_cloudfront_full_access = aws.iam.UserPolicyAttachment("hugo-cloudfront-full-access",
+hugo_cloudfront_full_access = aws.iam.UserPolicyAttachment(
+    "hugo-cloudfront-full-access",
     user=hugo_user.name,
     policy_arn="arn:aws:iam::aws:policy/CloudFrontFullAccess",
 )
 
 # CloudFront Distributions
-nelsonfigueroa_sh_cloudfront = aws.cloudfront.Distribution("nelsonfigueroa-sh",
+nelsonfigueroa_sh_cloudfront = aws.cloudfront.Distribution(
+    "nelsonfigueroa-sh",
     aliases=[
         "nelsonfigueroa.sh",
         "www.nelsonfigueroa.sh",
@@ -84,20 +92,22 @@ nelsonfigueroa_sh_cloudfront = aws.cloudfront.Distribution("nelsonfigueroa-sh",
     enabled=True,
     http_version="http2",
     is_ipv6_enabled=True,
-    origins=[{
-        "custom_origin_config": {
-            "http_port": 80,
-            "https_port": 443,
-            "origin_protocol_policy": "http-only",
-            "origin_ssl_protocols": [
-                "TLSv1",
-                "TLSv1.1",
-                "TLSv1.2",
-            ],
-        },
-        "domain_name": "nelsonfigueroa.sh.s3-website-us-west-1.amazonaws.com",
-        "origin_id": "S3-nelsonfigueroa.sh",
-    }],
+    origins=[
+        {
+            "custom_origin_config": {
+                "http_port": 80,
+                "https_port": 443,
+                "origin_protocol_policy": "http-only",
+                "origin_ssl_protocols": [
+                    "TLSv1",
+                    "TLSv1.1",
+                    "TLSv1.2",
+                ],
+            },
+            "domain_name": "nelsonfigueroa.sh.s3-website-us-west-1.amazonaws.com",
+            "origin_id": "S3-nelsonfigueroa.sh",
+        }
+    ],
     price_class="PriceClass_All",
     restrictions={
         "geo_restriction": {
@@ -109,17 +119,21 @@ nelsonfigueroa_sh_cloudfront = aws.cloudfront.Distribution("nelsonfigueroa-sh",
         "minimum_protocol_version": "TLSv1.2_2019",
         "ssl_support_method": "sni-only",
     },
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
 
-nelsonfigueroa_dev_distribution = aws.cloudfront.Distribution("nelsonfigueroa-dev",
+nelsonfigueroa_dev_distribution = aws.cloudfront.Distribution(
+    "nelsonfigueroa-dev",
     aliases=["nelsonfigueroa.dev"],
-    custom_error_responses=[{
-        "error_caching_min_ttl": 10,
-        "error_code": 404,
-        "response_code": 404,
-        "response_page_path": "/404.html",
-    }],
+    custom_error_responses=[
+        {
+            "error_caching_min_ttl": 10,
+            "error_code": 404,
+            "response_code": 404,
+            "response_page_path": "/404.html",
+        }
+    ],
     default_cache_behavior={
         "allowed_methods": [
             "GET",
@@ -138,16 +152,18 @@ nelsonfigueroa_dev_distribution = aws.cloudfront.Distribution("nelsonfigueroa-de
     enabled=True,
     http_version="http2",
     is_ipv6_enabled=True,
-    origins=[{
-        "custom_origin_config": {
-            "http_port": 80,
-            "https_port": 443,
-            "origin_protocol_policy": "http-only",
-            "origin_ssl_protocols": ["TLSv1.2"],
-        },
-        "domain_name": "nelsonfigueroa.dev.s3-website-us-west-1.amazonaws.com",
-        "origin_id": "S3-nelsonfigueroa.dev",
-    }],
+    origins=[
+        {
+            "custom_origin_config": {
+                "http_port": 80,
+                "https_port": 443,
+                "origin_protocol_policy": "http-only",
+                "origin_ssl_protocols": ["TLSv1.2"],
+            },
+            "domain_name": "nelsonfigueroa.dev.s3-website-us-west-1.amazonaws.com",
+            "origin_id": "S3-nelsonfigueroa.dev",
+        }
+    ],
     price_class="PriceClass_All",
     restrictions={
         "geo_restriction": {
@@ -159,17 +175,21 @@ nelsonfigueroa_dev_distribution = aws.cloudfront.Distribution("nelsonfigueroa-de
         "minimum_protocol_version": "TLSv1.2_2021",
         "ssl_support_method": "sni-only",
     },
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
 
-nelson_cloud_distribution = aws.cloudfront.Distribution("nelson-cloud",
+nelson_cloud_distribution = aws.cloudfront.Distribution(
+    "nelson-cloud",
     aliases=["nelson.cloud"],
-    custom_error_responses=[{
-        "error_caching_min_ttl": 10,
-        "error_code": 404,
-        "response_code": 404,
-        "response_page_path": "/404.html",
-    }],
+    custom_error_responses=[
+        {
+            "error_caching_min_ttl": 10,
+            "error_code": 404,
+            "response_code": 404,
+            "response_page_path": "/404.html",
+        }
+    ],
     default_cache_behavior={
         "allowed_methods": [
             "GET",
@@ -190,20 +210,32 @@ nelson_cloud_distribution = aws.cloudfront.Distribution("nelson-cloud",
     http_version="http2and3",
     is_ipv6_enabled=True,
     # route /gc/* requests to GoatCounter, stripping the /gc prefix via CloudFront Function
-    ordered_cache_behaviors=[{
-        "path_pattern": "/gc/*",
-        "allowed_methods": ["GET", "HEAD", "OPTIONS", "PUT", "PATCH", "POST", "DELETE"],
-        "cached_methods": ["GET", "HEAD"],
-        "target_origin_id": "goatcounter",
-        "viewer_protocol_policy": "redirect-to-https",
-        "compress": False,
-        "cache_policy_id": "4135ea2d-6df8-44a3-9df3-4b5a84be39ad",  # CachingDisabled
-        "origin_request_policy_id": "b689b0a8-53d0-40ab-baf2-68738e2966ac",  # AllViewerExceptHostHeader
-        "function_associations": [{
-            "event_type": "viewer-request",
-            "function_arn": goatcounter_rewrite.arn,
-        }],
-    }],
+    ordered_cache_behaviors=[
+        {
+            "path_pattern": "/gc/*",
+            "allowed_methods": [
+                "GET",
+                "HEAD",
+                "OPTIONS",
+                "PUT",
+                "PATCH",
+                "POST",
+                "DELETE",
+            ],
+            "cached_methods": ["GET", "HEAD"],
+            "target_origin_id": "goatcounter",
+            "viewer_protocol_policy": "redirect-to-https",
+            "compress": False,
+            "cache_policy_id": "4135ea2d-6df8-44a3-9df3-4b5a84be39ad",  # CachingDisabled
+            "origin_request_policy_id": "b689b0a8-53d0-40ab-baf2-68738e2966ac",  # AllViewerExceptHostHeader
+            "function_associations": [
+                {
+                    "event_type": "viewer-request",
+                    "function_arn": goatcounter_rewrite.arn,
+                }
+            ],
+        }
+    ],
     origins=[
         {
             "custom_origin_config": {
@@ -238,11 +270,13 @@ nelson_cloud_distribution = aws.cloudfront.Distribution("nelson-cloud",
         "minimum_protocol_version": "TLSv1.2_2021",
         "ssl_support_method": "sni-only",
     },
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
 
 # ACM Certificates
-nelsonfigueroa_sh_certificate = aws.acm.Certificate("nelsonfigueroa-sh",
+nelsonfigueroa_sh_certificate = aws.acm.Certificate(
+    "nelsonfigueroa-sh",
     domain_name="nelsonfigueroa.sh",
     key_algorithm="RSA_2048",
     options={
@@ -255,9 +289,11 @@ nelsonfigueroa_sh_certificate = aws.acm.Certificate("nelsonfigueroa-sh",
         "nelsonfigueroa.sh",
     ],
     validation_method="DNS",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelsonfigueroa_dev_certificate = aws.acm.Certificate("nelsonfigueroa-dev",
+nelsonfigueroa_dev_certificate = aws.acm.Certificate(
+    "nelsonfigueroa-dev",
     domain_name="nelsonfigueroa.dev",
     key_algorithm="RSA_2048",
     options={
@@ -270,9 +306,11 @@ nelsonfigueroa_dev_certificate = aws.acm.Certificate("nelsonfigueroa-dev",
         "nelsonfigueroa.dev",
     ],
     validation_method="DNS",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelson_cloud_certificate = aws.acm.Certificate("nelson-cloud",
+nelson_cloud_certificate = aws.acm.Certificate(
+    "nelson-cloud",
     domain_name="nelson.cloud",
     key_algorithm="RSA_2048",
     options={
@@ -285,116 +323,152 @@ nelson_cloud_certificate = aws.acm.Certificate("nelson-cloud",
         "nelson.cloud",
     ],
     validation_method="DNS",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
 # Route 53 Zones
-nelsonfigueroa_dev_zone = aws.route53.Zone("nelsonfigueroa-dev",
+nelsonfigueroa_dev_zone = aws.route53.Zone(
+    "nelsonfigueroa-dev",
     comment="",
     name="nelsonfigueroa.dev",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelson_cloud_zone = aws.route53.Zone("nelson-cloud",
+nelson_cloud_zone = aws.route53.Zone(
+    "nelson-cloud",
     comment="",
     name="nelson.cloud",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
 # Route 53 Records
 
 ## Records for nelsonfigueroa.dev
-nelsonfigueroa_dev_a = aws.route53.Record("nelsonfigueroa-dev-a",
-    aliases=[{
-        "evaluate_target_health": False,
-        "name": "d21xfi3nko5ufm.cloudfront.net",
-        "zone_id": "Z2FDTNDATAQYW2",
-    }],
+nelsonfigueroa_dev_a = aws.route53.Record(
+    "nelsonfigueroa-dev-a",
+    aliases=[
+        {
+            "evaluate_target_health": False,
+            "name": "d21xfi3nko5ufm.cloudfront.net",
+            "zone_id": "Z2FDTNDATAQYW2",
+        }
+    ],
     name="nelsonfigueroa.dev",
     type=aws.route53.RecordType.A,
     zone_id="Z05927031BXKMYS37I62Z",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelsonfigueroa_dev_aaaa = aws.route53.Record("nelsonfigueroa-dev-aaaa",
-    aliases=[{
-        "evaluate_target_health": False,
-        "name": "d21xfi3nko5ufm.cloudfront.net",
-        "zone_id": "Z2FDTNDATAQYW2",
-    }],
+nelsonfigueroa_dev_aaaa = aws.route53.Record(
+    "nelsonfigueroa-dev-aaaa",
+    aliases=[
+        {
+            "evaluate_target_health": False,
+            "name": "d21xfi3nko5ufm.cloudfront.net",
+            "zone_id": "Z2FDTNDATAQYW2",
+        }
+    ],
     name="nelsonfigueroa.dev",
     type=aws.route53.RecordType.AAAA,
     zone_id="Z05927031BXKMYS37I62Z",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelsonfigueroa_dev_mx = aws.route53.Record("nelsonfigueroa-dev-mx",
+nelsonfigueroa_dev_mx = aws.route53.Record(
+    "nelsonfigueroa-dev-mx",
     name="nelsonfigueroa.dev",
     records=["0 ."],
     ttl=300,
     type=aws.route53.RecordType.MX,
     zone_id="Z05927031BXKMYS37I62Z",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelsonfigueroa_dev_txt = aws.route53.Record("nelsonfigueroa-dev-txt",
+nelsonfigueroa_dev_txt = aws.route53.Record(
+    "nelsonfigueroa-dev-txt",
     name="nelsonfigueroa.dev",
-    records=["google-site-verification=Z6XialAUrcY8MKzEaYpfzsJ5YMg9k9Et11shkMS_ZWwl; v=spf1 -all;"],
+    records=[
+        "google-site-verification=Z6XialAUrcY8MKzEaYpfzsJ5YMg9k9Et11shkMS_ZWwl; v=spf1 -all;"
+    ],
     ttl=300,
     type=aws.route53.RecordType.TXT,
     zone_id="Z05927031BXKMYS37I62Z",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelsonfigueroa_dev_wildcard_txt = aws.route53.Record("nelsonfigueroa-dev-wildcard-txt",
+nelsonfigueroa_dev_wildcard_txt = aws.route53.Record(
+    "nelsonfigueroa-dev-wildcard-txt",
     name="*.nelsonfigueroa.dev",
     records=["v=spf1 -all"],
     ttl=300,
     type=aws.route53.RecordType.TXT,
     zone_id="Z05927031BXKMYS37I62Z",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelsonfigueroa_dev_acm_validation = aws.route53.Record("nelsonfigueroa-dev-acm-validation",
+nelsonfigueroa_dev_acm_validation = aws.route53.Record(
+    "nelsonfigueroa-dev-acm-validation",
     name="_1a4f4116967616f543b690fb21f46c65.nelsonfigueroa.dev",
     records=["_cfc95ae5fa66826ed893b0d493e04e1f.gjjrqxwdmz.acm-validations.aws."],
     ttl=300,
     type=aws.route53.RecordType.CNAME,
     zone_id="Z05927031BXKMYS37I62Z",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelsonfigueroa_dev_dmarc = aws.route53.Record("nelsonfigueroa-dev-dmarc",
+nelsonfigueroa_dev_dmarc = aws.route53.Record(
+    "nelsonfigueroa-dev-dmarc",
     name="_dmarc.nelsonfigueroa.dev",
     records=["v=DMARC1; p=reject;"],
     ttl=300,
     type=aws.route53.RecordType.TXT,
     zone_id="Z05927031BXKMYS37I62Z",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelsonfigueroa_dev_dkim = aws.route53.Record("nelsonfigueroa-dev-dkim",
+nelsonfigueroa_dev_dkim = aws.route53.Record(
+    "nelsonfigueroa-dev-dkim",
     name="*._domainkey.nelsonfigueroa.dev",
     records=["v=DKIM1; p="],
     ttl=300,
     type=aws.route53.RecordType.TXT,
     zone_id="Z05927031BXKMYS37I62Z",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
 ## Records for nelson.cloud
-nelson_cloud_a = aws.route53.Record("nelson-cloud-a",
-    aliases=[{
-        "evaluate_target_health": False,
-        "name": "d31hz6jsrwz1m1.cloudfront.net",
-        "zone_id": "Z2FDTNDATAQYW2",
-    }],
+nelson_cloud_a = aws.route53.Record(
+    "nelson-cloud-a",
+    aliases=[
+        {
+            "evaluate_target_health": False,
+            "name": "d31hz6jsrwz1m1.cloudfront.net",
+            "zone_id": "Z2FDTNDATAQYW2",
+        }
+    ],
     name="nelson.cloud",
     type=aws.route53.RecordType.A,
     zone_id="Z032177030KUZLOKGS50G",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelson_cloud_aaaa = aws.route53.Record("nelson-cloud-aaaa",
-    aliases=[{
-        "evaluate_target_health": False,
-        "name": "d31hz6jsrwz1m1.cloudfront.net",
-        "zone_id": "Z2FDTNDATAQYW2",
-    }],
+nelson_cloud_aaaa = aws.route53.Record(
+    "nelson-cloud-aaaa",
+    aliases=[
+        {
+            "evaluate_target_health": False,
+            "name": "d31hz6jsrwz1m1.cloudfront.net",
+            "zone_id": "Z2FDTNDATAQYW2",
+        }
+    ],
     name="nelson.cloud",
     type=aws.route53.RecordType.AAAA,
     zone_id="Z032177030KUZLOKGS50G",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelson_cloud_mx = aws.route53.Record("nelson-cloud-mx",
+nelson_cloud_mx = aws.route53.Record(
+    "nelson-cloud-mx",
     name="nelson.cloud",
     records=[
         "10 mx01.mail.icloud.com.",
@@ -403,9 +477,11 @@ nelson_cloud_mx = aws.route53.Record("nelson-cloud-mx",
     ttl=300,
     type=aws.route53.RecordType.MX,
     zone_id="Z032177030KUZLOKGS50G",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelson_cloud_txt = aws.route53.Record("nelson-cloud-txt",
+nelson_cloud_txt = aws.route53.Record(
+    "nelson-cloud-txt",
     name="nelson.cloud",
     records=[
         "apple-domain=shXv4LOydqcMHwRZ",
@@ -415,28 +491,35 @@ nelson_cloud_txt = aws.route53.Record("nelson-cloud-txt",
     ttl=300,
     type=aws.route53.RecordType.TXT,
     zone_id="Z032177030KUZLOKGS50G",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelson_cloud_acm_validation = aws.route53.Record("nelson-cloud-acm-validation",
+nelson_cloud_acm_validation = aws.route53.Record(
+    "nelson-cloud-acm-validation",
     name="_5870a8661430f6fdf996e15c8f998865.nelson.cloud",
     records=["_1e29dc944d393d448ca0f18f237ab593.stwyzjdjkd.acm-validations.aws."],
     ttl=300,
     type=aws.route53.RecordType.CNAME,
     zone_id="Z032177030KUZLOKGS50G",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelson_cloud_dmarc = aws.route53.Record("nelson-cloud-dmarc",
+nelson_cloud_dmarc = aws.route53.Record(
+    "nelson-cloud-dmarc",
     name="_dmarc.nelson.cloud",
     records=["v=DMARC1; p=none"],
     ttl=300,
     type=aws.route53.RecordType.TXT,
     zone_id="Z032177030KUZLOKGS50G",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-nelson_cloud_dkim = aws.route53.Record("nelson-cloud-dkim",
+nelson_cloud_dkim = aws.route53.Record(
+    "nelson-cloud-dkim",
     name="sig1._domainkey.nelson.cloud",
     records=["sig1.dkim.nelson.cloud.at.icloudmailadmin.com."],
     ttl=300,
     type=aws.route53.RecordType.CNAME,
     zone_id="Z032177030KUZLOKGS50G",
-    opts = pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
